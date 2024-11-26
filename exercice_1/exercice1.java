730422f5887aafe884475f382a8646211affafc9
MOUAFO NZOKOU TOURE NADER <mouafont@3il.fr>
2024-11-26 16:08:37 +0100

    Ajout de l'exercice1 de qualite de code

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,23 +1,48 @@
 public class FactureManager {
-
+	
+	/**
+	 * faire la reduction d'un montant suivant un pourcentage
+	 */
+	public double reduction(double montant, double pourcentage) {
+		return montant*(1-pourcentage);
+	}
+	
+	/**
+	 * faire la reduction d'un montant suivant la categorie
+	 */
+	public double reductionCategorie(String typeProduit,double montant) {
+		switch(typeProduit) {
+			case "Alimentaire"	:	montant=reduction(montant,0.05);
+									break;
+			case "Electronique"	:	montant=reduction(montant,0.1);
+									break;
+			case "Luxe"	:			montant=reduction(montant,0.15);
+									break;
+			default :				return montant;
+									break;
+		}
+		return moyenne;
+	}
+	
+	/**
+	 * faire la reduction suivant si le montant est important ou pas
+	 */
+	public double reductGrosMontant(double montant) {
+		if(montant>1000) {
+			montant=reduction(montant,0.05);//Reduction de 5%
+		}
+		return montant;
+	}
+	
+	
     // Calcul le total d'une facture
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
         double total = quantite * prixUnitaire;
 
         // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
-        }
-
+        total=reductionCategorie(typeProduit);
         // Reduction sur le total
-        if (total > 1000) {
-            total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
-        }
-
+        total=reductGrosMontant(total);
         return total;
     }
 }
